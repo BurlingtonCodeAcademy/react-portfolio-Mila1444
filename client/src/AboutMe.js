caa12e99211dbcf2d6af6c48dec697f99908ab69
Mila1444 <66134690+Mila1444@users.noreply.github.com>
2020-07-17 16:30:05 -0400

    added about page

--- client/src/AboutMe.js
+++ client/src/AboutMe.js
@@ -5,22 +5,21 @@ function AboutMe(props) {
     return (
         <div id='main-container'>
             <div id='body'>
-                <h3 id='content-home'> Peruvian loving Vermont! </h3>
-                <div>
-                    <p id='p-home'> Originally I am from Lima, Peru - I've been living in Vermont for about 8 years now. I learned about The University of Vermont while attending my fouth year in Architecture at the Pontificia Universidad Catolica del Peru - XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX</p>
-                    <p id='p-home'> Originally I am from Lima, Peru - I've been living in Vermont for about 8 years now. I learned about The University of Vermont while attending my fouth year in Architecture at the Pontificia Universidad Catolica del Peru - XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-                    XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-                    XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-                    XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX</p>
+                <h3 id='h3-about'> Peruvian loving Vermont! </h3>
+                <div id='content-home'>
+                    <p> Originally I am from Lima, Peru - I've been living in Vermont for about 8 years now. I learned about The University of Vermont while attending my fouth year in Architecture at the Pontificia Universidad Catolica del Peru</p>
+                    <p> Originally I am from Lima, Peru - I've been living in Vermont for about 8 years now. I learned about The University of Vermont while attending my fouth year in Architecture at the Pontificia Universidad Catolica del Peru</p>
                 </div>
             </div>
             <div id='sidebar'>
-                <h3 id='h3-sidebar'> Would you like to know more about Peru?</h3>
-                <div>
+                <div id='sidebar-content'>
+                    <h3 id='h3-sidebar'> Have you ever visited Peru?</h3>
                     <h4>You would love MachuPicchu... and the llamas!</h4>
-                    <p>If you ever visit Peru, don't miss Machu Picchu, one of the most important and fascinating Inca citadels, built in the 15th century</p>
-                    <img src={Machu} />
-                    <img src={Machu} />
+                    <p>If you ever visit Peru, don't miss Machu Picchu, one of the most important and fascinating Inca citadels, built in the 15th century.</p>
+                </div>
+                <div id='sidebar-image-container'>
+                    <img id='image-sidebar' src={Machu} />
+                    <img id='image-sidebar' src={Machu} />
                 </div>
             </div>
         </div>
