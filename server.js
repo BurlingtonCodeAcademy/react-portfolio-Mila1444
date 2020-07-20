c6fb319677a9992843c5b2a3793ae7918a8f6fc9
Mila1444 <66134690+Mila1444@users.noreply.github.com>
2020-07-20 09:28:55 -0400

    fixing server

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-const express = require('express')
+const express = require('express');
+const path = require('path')
+const port = process.env.port || 5000
+
+const app = express()
+
+app.use(express.static(path.resolve('./client/build')))
+
+app.get('*', (req, res) => {
+    res.sendFile('./client/build/index.html')
+})
+
+app.listen(port,() => {
+    console.log('Listening on port:', port)
+})
